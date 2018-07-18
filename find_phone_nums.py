07d97ca30c8ae5385fa37b33e7b66e13570c86b1
lucafranklin <lucafranklin@gmail.com>
2018-07-17 18:01:44 -0700

    added regex

--- find_phone_nums.py
+++ find_phone_nums.py
@@ -4,26 +4,15 @@ import sys
 import re
 
 def isPhoneNumber(text):
-	if len(text) != 12:
-		return False
-	for i in range (0,3):
-		if not text[i].is_decimal:
-			return False
-	if text[3] != '-':
-		return False
-	for i in range (4,7):
-		if not text[i].is_decimal:
-			return False
-	if text[7] != '-':
-		return False
-	for i in range (8,12):
-		if not text[i].is_decimal:
-			return False
-	else:
-		return True
+	#regex for phone numbers
+	phone_regexes = re.compile(r'\d{3}-\d{3}-\d{4}')
+	
+	nums = phone_regexes.findall(text)
+	return nums
 		
 def read_file():
-	while True:
+	textcontent = ""
+	while not textcontent:
 		textfilepath = input('\nEnter file path for text file: ')
 		try:
 			textfile = open(textfilepath)
@@ -31,21 +20,12 @@ def read_file():
 			textfile.close()
 			return textcontent
 		except:
-			print('Incorrect file path. Try again.\n')
+			print('\nIncorrect file path. Try again.')
 		
-def main():
-	
-	numbers = []
-	
+def main():	
 	print('\nThis program scans text files and prints out found phone numbers.\n')
 	file = read_file()
-	file.replace("\n", " ")
-	wordlist = file.split(" ")
-	for word in wordlist:
-		if isPhoneNumber(word):
-			numbers.append(word)
-		else:
-			continue
-	print numbers
+	numbers = isPhoneNumber(file)
+	print(numbers)
 	
 main()	
