c99ca2d20f067ed80c04395b141876539be39d6e
lucafranklin <lucafranklin@gmail.com>
2018-08-01 13:54:53 -0700

    added code

--- look_busy.py
+++ look_busy.py
@@ -4,5 +4,15 @@ import pyautogui
 import time
 
 def check_mouse_movement():
-	
+	xold, yold = pyautogui.position()
+	time.sleep(10)
+	if xold, yold != pyautogui.position():
+		return False
+	else:
+		return True
+
+while True:
+	if not check_mouse_movement():
+		pyautogui.moveRel(10, 0, duration = 0.1)
+		pyautogui.moveRel(-10, 0, duration = 0.1)
 
