2f3b9bf47947c04ccf8156baab7dc9bd09846248
lucafranklin <lucafranklin@gmail.com>
2018-08-13 14:06:38 -0700

    Program for scraping xkcd comics

---
+++ download_xkcd.py
@@ -0,0 +1,41 @@
+# download_xkcd.py - Downloads every single XKCD comic (From "Automate the Boring Stuff with Python").
+
+import requests, os, bs4
+
+url = 'http://xkcd.com' #starting url
+
+os.makedirs('xkcd', exist_ok=True) #Store comics in ./xkcd
+
+while not url.endswith('#'):
+
+	#TODO: Download the Page.
+	print('Downloading page %s...' % url)
+	res = requests.get(url)
+	res.raise_for_status()
+	
+	soup = bs4.BeautifulSoup(res.text)
+	
+	#TODO: Find URL of comic image.
+	comicElem = soup.select('#comic img')
+	if comicElem == []:
+		print('Could not find comic image.')
+	else:
+		comicUrl = 'http:' + comicElem[0].get('src')
+		
+	#TODO: Download the image./
+	print('Downloading image %s...' % (comicUrl))
+	res = requests.get(comicUrl)
+	res.raise_for_status()
+	
+	#TODO: Save image to ./xkcd
+	imageFile = open(os.path.join('xkcd',os.path.basename(comicUrl)), 'wb')
+	for chunk in res.iter_content(100000):
+		imageFile.write(chunk)
+	imageFile.close()
+	
+	#TODO: Get Prev button's url
+	prevLink = soup.select('a[rel="prev"]') [0]
+	url = 'http://xkcd.com' + prevLink.get('href')
+	
+	
+print('Done.')
